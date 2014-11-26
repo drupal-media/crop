cd806668f42b146e83fe74a2cd71957a1a84de9e
Janez Urevc <janez@janezurevc.name>
2014-11-26 22:08:35 +0100

    Issue #2376695 by slashrsm: Fix typo.

--- crop.api.php
+++ crop.api.php
@@ -14,7 +14,7 @@
  * Alter the information provided in \Drupal\crop\Annotation\CropEntityProvider.
  *
  * @param $providers
- *   The array of provider plugins, keyed on the machine-readable name.
+ *   The array of provider plugins, keyed by the machine-readable name.
  */
 function hook_crop_entity_provider_info_alter(&$providers) {
   $providers['media']['label'] = t('Super fancy provider for media entity!');
