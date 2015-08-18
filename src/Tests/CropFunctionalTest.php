bbdf77f604ce774c4750399943d22c8d32d11f6b
Dave Reid <dave@davereid.net>
2015-08-18 09:52:47 -0500

    Fixed String component renamed to SafeMarkup.

--- src/Tests/CropFunctionalTest.php
+++ src/Tests/CropFunctionalTest.php
@@ -7,7 +7,7 @@
 
 namespace Drupal\crop\Tests;
 
-use Drupal\Component\Utility\String;
+use Drupal\Component\Utility\SafeMarkup;
 use Drupal\simpletest\WebTestBase;
 
 /**
@@ -91,7 +91,7 @@ class CropFunctionalTest extends WebTestBase {
     $this->assertRaw(t('The crop type %name has been added.', ['%name' => $edit['label']]));
     $this->assertUrl('admin/structure/crop');
     $label = $this->xpath("//td[contains(concat(' ',normalize-space(@class),' '),' menu-label ')]");
-    $this->assert(strpos($label[0]->asXML(), String::checkPlain($edit['label'])) !== FALSE, 'Crop type label found on listing page.');
+    $this->assert(strpos($label[0]->asXML(), SafeMarkup::checkPlain($edit['label'])) !== FALSE, 'Crop type label found on listing page.');
     $this->assertText($edit['description']);
 
     // Check edit form.
@@ -104,7 +104,7 @@ class CropFunctionalTest extends WebTestBase {
     // See if crop type appears on image effect configuration form.
     $this->drupalGet('admin/config/media/image-styles/manage/' . $this->testStyle->id() . '/add/crop_crop');
     $option = $this->xpath("//select[@id='edit-data-crop-type']/option");
-    $this->assert(strpos($option[0]->asXML(), String::checkPlain($edit['label'])) !== FALSE, 'Crop type label found on image effect page.');
+    $this->assert(strpos($option[0]->asXML(), SafeMarkup::checkPlain($edit['label'])) !== FALSE, 'Crop type label found on image effect page.');
     $this->drupalPostForm('admin/config/media/image-styles/manage/' . $this->testStyle->id() . '/add/crop_crop', ['data[crop_type]' => $edit['id']], t('Add effect'));
     $this->assertText(t('The image effect was successfully applied.'));
     $this->assertText(t('Manual crop uses @name crop type', ['@name' => $edit['label']]));
